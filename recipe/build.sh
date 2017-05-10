e0cdb4400f5f1511514707d004aa4ff98ac01035
Johannes Ring <johannr@simula.no>
2017-05-10 15:22:18 +0200

    FindBoost.cmake has been removed upstream

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,9 +11,6 @@ pip install --no-deps --no-binary :all: -r "${RECIPE_DIR}/component-requirements
 
 # DOLFIN
 
-# use conda cmake version for 1.63 detection
-rm cmake/modules/FindBoost.cmake
-
 rm -rf build
 mkdir build
 cd build
