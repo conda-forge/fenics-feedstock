f5103c8a4a258af7bacf44833d7fee86b8b9a57a
Min RK <benjaminrk@gmail.com>
2018-06-26 18:14:39 +0200

    update tests for 2018.1

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -8,15 +8,12 @@ fi
 
 export DIJITSO_CACHE_DIR=${PWD}/instant
 
-pushd "test/unit/python"
+pushd "python/test/unit"
 TESTS="jit fem/test_form.py::test_assemble_linear"
 
 RUN_TESTS="python -b -m pytest -vs $TESTS"
 # serial
 $RUN_TESTS
-# parallel
 
-if [[ "$(uname)" == "Darwin" ]]; then
-  # FIXME: skip mpi tests on Linux pending conda-smithy fix #337
-  mpiexec -n 3 $RUN_TESTS
-fi
+# parallel
+mpiexec -n 3 $RUN_TESTS 2>&1 </dev/null | cat
