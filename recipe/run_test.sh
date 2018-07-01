196c14625c9e2fe51c2621c7a2722a1a48d91a1e
Min RK <benjaminrk@gmail.com>
2018-07-01 13:16:24 +0200

    check for dolfin features in tests

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -8,6 +8,19 @@ fi
 
 export DIJITSO_CACHE_DIR=${PWD}/instant
 
+# verify that we have the features we intend to
+python <<EOF
+import dolfin
+dolfin.info(dolfin.parameters["form_compiler"], True)
+assert dolfin.has_hdf5(), 'hdf5'
+assert dolfin.has_petsc(), 'petsc'
+assert dolfin.has_petsc4py(), 'petsc4py'
+assert dolfin.has_slepc(), 'slepc'
+assert dolfin.has_scotch(), 'scotch'
+assert dolfin.has_mpi(), 'mpi'
+assert dolfin.has_parmetis(), 'parmetis'
+EOF
+
 python -c 'from dolfin import *; info(parameters["form_compiler"], True)'
 
 pushd "python/test/unit"
