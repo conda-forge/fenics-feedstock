9412a0b764b1ad83e9cd61f546a381d213bbefa4
Isuru Fernando <isuruf@gmail.com>
2019-06-07 20:40:29 -0500

    unset CMAKE_PREFIX_PATH

--- recipe/build-libdolfin.sh
+++ recipe/build-libdolfin.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 set -eux
 
-cd dolfin
+unset CMAKE_PREFIX_PATH
 
-if [[ "$(uname)" == "Darwin" ]]; then
-  export MACOSX_DEPLOYMENT_TARGET=10.9
-  export CXXFLAGS="-std=c++11 -stdlib=libc++ $CXXFLAGS"
-  export LDFLAGS="-Wl,-rpath,$PREFIX/lib $LDFLAGS"
-fi
+cd dolfin
 
 # scrub problematic -fdebug-prefix-map from C[XX]FLAGS
 # these are loaded in the clang[++] activate scripts
