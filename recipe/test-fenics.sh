cea131588184ce9a2b04584f84a1d03d9192d90a
Min RK <benjaminrk@gmail.com>
2022-04-08 13:29:43 +0200

    script_env doesn't apply to imports

---
+++ recipe/test-fenics.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+
+if [[ "$(uname)" == "Darwin" ]]; then
+  export MACOSX_DEPLOYMENT_TARGET=10.9
+  export CXXFLAGS="-std=c++11 -stdlib=libc++ $CXXFLAGS"
+fi
+
+mpiexec="mpiexec"
+if command -v "${PREFIX}/bin/ompi_info" >/dev/null; then
+    export OMPI_MCA_plm=isolated
+    export OMPI_MCA_rmaps_base_oversubscribe=yes
+    export OMPI_MCA_btl_vader_single_copy_mechanism=none
+    mpiexec="mpiexec --allow-run-as-root"
+fi
+
+export DIJITSO_CACHE_DIR=${PWD}/instant
+
+# verify that we have the features we intend to
+python <<EOF
+import dolfin
+dolfin.info(dolfin.parameters["form_compiler"], True)
+assert dolfin.has_hdf5(), 'hdf5'
+assert dolfin.has_hdf5_parallel(), 'parallel hdf5'
+assert dolfin.has_petsc(), 'petsc'
+assert dolfin.has_petsc4py(), 'petsc4py'
+assert dolfin.has_slepc(), 'slepc'
+assert dolfin.has_scotch(), 'scotch'
+assert dolfin.has_mpi(), 'mpi'
+assert dolfin.has_parmetis(), 'parmetis'
+EOF
